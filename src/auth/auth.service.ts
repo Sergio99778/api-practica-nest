54e5b568201412f470300c90cef831750116e3ef
Sergio99778 <sergio4brandon@gmail.com>
2022-05-18 18:09:05 -0500

    Pipes working globally.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  signIn() {
+    return 'I am signing in';
+  }
+
+  signUp() {
+    return 'I am signing up';
+  }
+}
